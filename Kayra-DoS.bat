167fc74b43b95eaeff5606d25042084036828c15
MLKisRolling <94867695+alpkayra5@users.noreply.github.com>
2023-01-28 16:24:34 +0300

    project
    my repo

---
+++ Kayra-DoS.bat
@@ -0,0 +1,193 @@
+@echo off
+echo Kayra-DoS
+echo Select your launguage
+echo 1.Turkish
+echo 2.English
+set input=nothing
+set/p input=Launguage:
+if %input%==1 goto Turkish
+if %input%==2 goto English 
+:English
+echo Starting Kayra-DoS
+timeout /t 3 >nul
+cls
+color 97
+echo searching all disks
+echo (.)
+timeout /t 1 >nul
+cls
+echo (..)
+timeout /t 1 >nul
+cls
+echo (...)
+timeout /t 1 >nul
+cls
+echo (....)
+timeout /t 1 >nul
+cls
+echo (................)
+timeout /t 1 >nul
+cls
+echo (.....................)
+timeout /t 1 >nul
+cls
+echo (................................)
+cls
+echo Insert disc to continue installation & pause >nul
+echo (.)
+timeout /t 5 >nul
+cls
+echo (..)
+timeout /t 5 >nul
+cls
+echo (...)
+timeout /t 5 >nul
+cls
+echo (....)
+timeout /t 5 >nul
+cls
+echo (.....)
+timeout /t 5 >nul
+cls
+echo (........)
+timeout /t 5 >nul
+cls
+echo (.............)
+timeout /t 5 >nul
+cls
+echo (...............)
+timeout /t 5 >nul
+cls
+echo (..................................)
+cls
+timeout /t 5 >nul
+cls
+echo Ýnstallation Completed!.!.!.!.!.!.!.!.!.!.!.!............................................................
+ timeout /t 2 >nul
+cls
+echo GUI DOS START EN AN TR LIGHT COMPLETED AN SOURCES LICENSE HAS DOS COPYED DOS SYSTEM AND WRITED LIGHT FILES AND USE SYSTEM.
+:tekrar
+echo 1.Calculator
+echo 2.System info
+echo 3.Task manager
+set input=nothing
+set/p input=System$
+if %input%==1 goto HesapMakinesi
+if %input%==2 goto info
+if %input%==3 goto task
+:HesapMakinesi
+set /p sum=Ýslem=
+set /a ans=%sum%
+echo.
+echo = %ans%
+pause
+cls
+goto Tekrar
+:info
+echo Your system info:Name= Kayra-DoS-RAM=5 mb ram-BÝT=16
+pause
+cls
+goto Tekrar
+:task
+echo Your all tasks=
+echo sysinfoo.pul
+echo Fclciker.pul
+echo taskmngr.pul
+echo antivirus.pul
+echo systemlan.dil
+pause
+cls
+goto Tekrar
+:Turkish
+echo Kayra-DoS baslatiliyor
+timeout /t 3 >nul
+cls
+color 97
+echo Tüm diskler taraniyor
+echo (.)
+timeout /t 1 >nul
+cls
+echo (..)
+timeout /t 1 >nul
+cls
+echo (...)
+timeout /t 1 >nul
+cls
+echo (....)
+timeout /t 1 >nul
+cls
+echo (................)
+timeout /t 1 >nul
+cls
+echo (.....................)
+timeout /t 1 >nul
+cls
+echo (................................)
+cls
+timeout /t 5 >nul
+cls
+echo Devam etmek icin diski takin & pause >nul
+echo (.)
+timeout /t 5 >nul
+cls
+echo (..)
+timeout /t 5 >nul
+cls
+echo (...)
+timeout /t 5 >nul
+cls
+echo (....)
+timeout /t 5 >nul
+cls
+echo (.....)
+timeout /t 5 >nul
+cls
+echo (........)
+timeout /t 5 >nul
+cls
+echo (.............)
+timeout /t 5 >nul
+cls
+echo (...............)
+timeout /t 5 >nul
+cls
+echo (..................................)
+cls
+timeout /t 5 >nul
+cls
+echo Ýndirme tamamlandi!.!.!.!.!.!.!.!.!.!.!.!............................................................
+timeout /t 2 >nul
+cls
+echo GUI DOS START EN AN TR LIGHT COMPLETED AN SOURCES LICENSE HAS DOS COPYED DOS SYSTEM AND WRITED LIGHT FILES AND USE SYSTEM.
+:tekrar2
+echo 1.HesapMakinesi
+echo 2.Sistem bilgisi
+echo 3.Gorev Yöneticisi
+set input=nothing
+set/p input=Sistem$
+if %input%==1 goto HesapMakinesi
+if %input%==2 goto Bilgi
+if %input%==3 goto görev
+:HesapMakinesi
+set /p sum=Ýslem=
+set /a ans=%sum%
+echo.
+echo = %ans%
+pause
+cls
+goto tekrar2
+:Bilgi
+echo Sizin sistem bilginiz:AD= Kayra-DoS-RAM=5 mb ram-MÝMARÝ=16
+pause
+cls
+goto tekrar2
+:görev
+echo Tüm görevleriniz=
+echo sysinfoo.pul
+echo Fclciker.pul
+echo taskmngr.pul
+echo antivirus.pul
+echo systemlan.dil
+pause
+cls
+goto tekrar2
